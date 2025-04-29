e06e46459a0bcf97cc28211f81f27f70d5a28cc8
Gustavo-Jodar <Gustavo-Jodar@users.noreply.github.com>
2025-04-29 13:41:42 +0000

    deploy: 4364ca38d7af8db96c96a4d041ef038a272d80fb

--- search/functions_d.js
+++ search/functions_d.js
@@ -2,6 +2,5 @@ var searchData=
 [
   ['os_5fget_5furi_5fheader_0',['os_get_uri_header',['../nfc__ndef_8h.html#ad3b822d9a96dbb03a2015ac0e06e8cf3',1,'nfc_ndef.h']]],
   ['os_5fndef_5fto_5fstring_1',['os_ndef_to_string',['../nfc__ndef_8h.html#afcf247d6f1483900066313636758ecf1',1,'nfc_ndef.h']]],
-  ['os_5fndef_5fto_5fstring2_2',['os_ndef_to_string2',['../nfc__ndef_8h.html#a0f81cab0f758c71d35273f711f442883',1,'nfc_ndef.h']]],
-  ['os_5fparse_5fndef_3',['os_parse_ndef',['../nfc__ndef_8h.html#a6c85d6248ba648dcaa49a0d5eb10d76e',1,'nfc_ndef.h']]]
+  ['os_5fparse_5fndef_2',['os_parse_ndef',['../nfc__ndef_8h.html#a6c85d6248ba648dcaa49a0d5eb10d76e',1,'nfc_ndef.h']]]
 ];
